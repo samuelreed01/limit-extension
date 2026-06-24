c14f13e3a4392630ecc046c25ff0d32be2fbd6b1
Samuel Reed <samuelreed01@gmail.com>
2026-06-24 13:45:36 +0700

    update ui

--- popup/dialog.js
+++ popup/dialog.js
@@ -1,6 +1,9 @@
 import { defaultLimit, formatDomain, limitDateKey, limitsByDomainKey, usageByDomainKey } from '../constants.js';
 
 window.addEventListener('load', async () => {
+  const generalData = document.getElementById('general-data');
+  const siteSpecificData = document.getElementById('site-specific-data');
+
   const settingsButton = document.getElementById('settings-button');
   if (settingsButton) {
     settingsButton.addEventListener('click', () => {
@@ -26,6 +29,11 @@ window.addEventListener('load', async () => {
   const limitDate = storage[limitDateKey];
   const limitUsed = storage[usageByDomainKey]?.[domain]?.length ?? 0;
   const limit = storage[limitsByDomainKey]?.[domain] ?? defaultLimit;
+
+  if (limit && generalData && siteSpecificData) {
+    generalData.style.display = 'none';
+    siteSpecificData.style.display = 'block';
+  }
 
   const totalSpan = document.querySelector('.total-span');
   const limitSpan = document.querySelector('.limit-span');
